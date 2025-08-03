3b5bfd4f8c765da8f795281f6307cd3de21b1417
DustedAsh3 <ashbyki@gmail.com>
2025-08-02 23:27:31 -0600

    2025080202
    Fixed Save Button

--- debug/player_debug_button.gd
+++ debug/player_debug_button.gd
@@ -1,12 +1,7 @@
-extends Button
-
-
-
-func _on_pressed() -> void:
-	SaveManager.save_game()
+extends Control
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_P):
-		_on_pressed()
+		SaveManager.save_game()
 	if Input.is_key_pressed(KEY_L):
 		SaveManager.load_game()
